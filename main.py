0155fc49e4e031fe01aac074e3e6dd6ecc4990bf
Vishnu VS <vishnuvs.it@gmail.com>
2025-12-14 17:05:24 +0530

    Added new API post methed

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class   post(BaseModel):
+    title: str
+    content: str
+    published:bool = True
+    rating:Optional[int] = None
+
 
 
 @app.get("/")
@@ -9,8 +19,15 @@ def root():
 
 @app.get("/posts")
 def posts():
-    return {"data": "this is your post data"}
+    return {"data": "this is your posts "}
 
+#post method url : "/data"
 
+@app.post("/createposts")
+def create_posts(new_post:post ):
+    print(new_post)
+    print(new_post.dict())
+    return {"data": new_post}
 
+#title str, content str
 
